92c86b138e3567c688711395cbea9d866a401c0b
TinaHsing <tina@quantaser.com>
2017-03-01 21:24:38 +0800

    the same with previous change

--- libraries/LTC2451/LTC2451.h
+++ libraries/LTC2451/LTC2451.h
@@ -1,18 +1,21 @@
 #define LTC2451ADD 0x14
+#define LTC2451ADD_SWRITE 0x28 // For softi2c communication 0x14 << 1
+#define LTC2451ADD_SREAD 0x29
 #define I2CREAD 1
 #define I2CWRITE 0
-#include <SoftI2C.h>
+
+#include <I2CMaster.h>
 #include <Wire.h>
-class SoftI2C;
+class SoftI2cMaster;
 class LTC2451
 {
 	public:
 		LTC2451();
-		void Init();
-		bool SoftI2CInit(unsigned char, unsigned char);
+		void Init(unsigned char);
+		bool SoftI2CInit(unsigned char, unsigned char, unsigned char);
 		unsigned int Read();
 		unsigned int SoftI2CRead();
 	private:
 		unsigned char g_sdapin, g_sclpin;
-		SoftI2C softi2c;
+		SoftI2cMaster softi2c;
 };
