7028602e9ac1e01ffdf1db55b1f31791560c2c87
TinaHsing <tina@quantaser.com>
2017-03-01 21:33:31 +0800

    still minor change

--- libraries/LTC2451/examples/LTC2451_SoftI2CRead/LTC2451_SoftI2CRead.ino
+++ libraries/LTC2451/examples/LTC2451_SoftI2CRead/LTC2451_SoftI2CRead.ino
@@ -6,26 +6,19 @@
 
 #define SDA_PIN 4
 #define SCL_PIN 3
-
-
 LTC2451 adc;
 void setup() {
 
   Serial.begin(9600);
-  Serial.println(adc.SoftI2CInit(4,3, MODE30HZ));
+  Serial.println(adc.SoftI2CInit(SDA_PIN, SCL_PIN, MODE30HZ));
   delay(35); // 30Hz mode, next request should be later then 33ms later!!
-  
-
-}
-
+ }
 void loop() {
- 
   unsigned int x; 
   x =adc.SoftI2CRead();
   Serial.print("adc:");
   Serial.println(x);
   delay(35);
-
 }
 
 
