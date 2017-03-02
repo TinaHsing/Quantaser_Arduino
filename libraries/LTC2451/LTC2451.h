914129b612e9eab9e04af189c14aa765c706ca28
adam1025 <adam@quantaser.com>
2017-03-02 17:00:13 +0800

    add conversion time wait into each read mode

--- libraries/LTC2451/LTC2451.h
+++ libraries/LTC2451/LTC2451.h
@@ -1,10 +1,15 @@
+#ifndef LTC2451_H
+#define LTC2451_H
+
 #define LTC2451ADD 0x14
 #define LTC2451ADD_SWRITE 0x28 // For softi2c communication 0x14 << 1
 #define LTC2451ADD_SREAD 0x29
 #define I2CREAD 1
 #define I2CWRITE 0
+#define MODE30HZ 1
+#define MODE60HZ 0
 
-#include <I2CMaster.h>
+#include <I2cMaster.h>
 #include <Wire.h>
 class SoftI2cMaster;
 class LTC2451
@@ -16,6 +21,8 @@ class LTC2451
 		unsigned int Read();
 		unsigned int SoftI2CRead();
 	private:
-		unsigned char g_sdapin, g_sclpin;
+		unsigned char g_sdapin, g_sclpin, p_waitTime;
+		unsigned int p_timeStart, p_vout;
 		SoftI2cMaster softi2c;
 };
+#endif
