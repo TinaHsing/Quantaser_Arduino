de73373545ecfc73b3ce7ab68fa011b8b64e8ac4
adam1025 <adam@quantaser.com>
2017-04-05 20:51:23 +0800

    modify

--- PZTDRF/PZTDRF.ino
+++ PZTDRF/PZTDRF.ino
@@ -2,10 +2,10 @@
 #include <SawtoothGen.h>
 #define AI_PIN A2
 
-led4X7_disp led(2,3,4,5,8,9,10,11,13); // (A,B,C,D,0,1,2,3,h), A~D: BCD pins, 0~3: position pins, h : point
+led4X7_disp led(2,3,4,5,8,9,10,11,12); // (A,B,C,D,0,1,2,3,h), A~D: BCD pins, 0~3: position pins, h : point
 SawtoothGen fn_gen(1, 6);// comparator AI pin, charge control pin
 void setup() {
-  led.init(2, 10, 31.1); // AI pin; adc bits, option : 10, 12, 16; gain 
+  led.init(0, 10, 31.1); // AI pin; adc bits, option : 10, 12, 16; gain 
 }
 
 void loop() {
