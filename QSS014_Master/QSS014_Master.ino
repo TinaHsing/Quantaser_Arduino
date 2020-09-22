1e2e2aae64cb6b633df92a4565dab0cd0af2c054
TinaHsing <tina@quantaser.com>
2020-09-22 14:53:43 +0800

    fixed the float digit bug

--- QSS014_Master/QSS014_Master.ino
+++ QSS014_Master/QSS014_Master.ino
@@ -34,7 +34,6 @@ void setup() {
 
   cmd_list[2].cmd = "READ_ADC";
   cmd_list[2].action = ACT_readAdc;
-  // heloo
 
 
 /****************************************************/
@@ -92,7 +91,7 @@ void ACT_readAdc(char *string)
    unsigned int value;
 
    value = adc.Read();
-   Serial.println(float(value)*ADC_REF/65535.0, 5);
+   Serial.println(value);
 }
 
 
